44df2be91ed8ebd048c7a31c5e1d9241b9643501
daniel <danielaraux@hotmail.com>
2025-06-16 14:35:54 +0200

    first-commit

---
+++ src/exercicesAnswers.js
@@ -0,0 +1,69 @@
+//Exercice 1 : Créer un tableau days et lui ajouter les jours de la semaine.
+let createArray = function (){
+  // ici votre return
+}
+
+//Exercice 2 : Retouner le 3ème élément du tableau days', function() {
+let returnThirdElement = function (array){
+  // ici votre return
+}
+
+//Exercice 3 : Afficher la valeur de l\'index 3 du tableau days.', function() {
+let returnThirdIndex = function (array){
+  // ici votre return
+}
+
+//Exercice 4 : Changer le 2ème élément du tableau languages par php.', function() {
+let changeSecondElement = function (array){
+  // ici votre return
+}
+
+//Exercice 5 : Ajouter ruby et python à la fin du tableau languages.', function() {
+let addAtTheEnd = function (array){
+  // ici votre return
+}
+
+//Exercice 6 : Ajouter html et css au début du tableau languages.', function() {
+let addAtFirst = function (array){
+  // ici votre return
+}
+
+//Exercice 7 : Supprimer le premier élément du tableau languages.', function() {
+let removeFirstElement = function (array){
+  // ici votre return
+}
+
+//Exercice 8 : Supprimer le dernier élément du tableau languages.', function() {
+let removeLastElement = function (array){
+  // ici votre return
+}
+
+//Exercice 9 : Créer un tableau associatif person. Lui ajouter les index name, age et city avec les valeurs Jean, 45, Paris.', function() {
+let createPerson = function (){
+  // ici votre return
+}
+
+//Exercice 10 : Retourner la valeur de l\'index nom du tableau person.', function() {
+let returnName = function (array){
+  // ici votre return
+}
+
+//Exercice 11 : Stocker tous les index du tableau person dans des variables séparées et les retourner dans une même phrase de type : "Bonjour, je suis Jean. J\'ai 45 ans et j\'habite Paris."', function() {
+let returnSentence = function (array){
+  // ici votre return
+}
+
+//Exercice 12 : Transformer la variable string en tableau.', function() {
+let returnArray = function (string){
+  // ici votre return
+}
+
+//Exercice 13 : Trier le tableau array.', function() {
+let sortArray = function (array){
+  // ici votre return
+}
+
+//Exercice 14 : Transformer le tableau array en chaîne de caractères', function() {
+let returnString = function (array){
+  // ici votre return
+}
